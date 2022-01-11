4d8378b15800fd20d09d7c5aae04f9d6c79e0a97
kofi-teddy <agudogot@gmail.com>
2022-01-11 11:22:19 +0000

    updated navs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="mt-0 bg-main">
+    <div>
+      <div>
+        <div class="flex items-center justify-center flex-auto">
+          <div class="w-full max-w-sm p-10 bg-green-600 rounded-3xl rounded-t-none">
+            <div class="mt-1 text-sm text-gray-50 text-center">
+              <span class="mx-1"><a href="/" target="_blank">Kofi Teddy</a></span>
+              <span class="mx-1"><a href="#!" target="_blank">Blog</a></span>
+              <span class="mx-1"><a href="#!" target="_blank">Projects</a></span>
+              <span class="mx-1"><a href="#!" target="_blank">Contact</a></span>
+            </div>     
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="pt-8 mt-20">
       <div class="mt-5">
         <div class="flex items-center justify-center flex-auto">
